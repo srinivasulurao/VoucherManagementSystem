625759a7b75b51cbbec116a7bf17e079f2035323
srinivasulurao <n.srinivasulurao@gmail.com>
2019-12-23 01:18:55 +0530

    Paypal Field Added

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -18,15 +18,20 @@ export class MyProfileComponent implements OnInit {
   public plans:any;
   public alertClass:any;
   public alertMessage:any;
+  public company_logo:any;
+  public button_text:any;
+  public company_logo_temp:any;
 
   public CompanyNameControl=new FormControl('',[Validators.required]);
   public PlanControl=new FormControl('',[Validators.required]); 
   public CurrencyControl=new FormControl('',[Validators.required]); 
+  public PayPalEmailControl=new FormControl('', [Validators.required, emailValidate]); 
   public CompanyEmailControl=new FormControl({disabled:'true'},[Validators.required, emailValidate]); //Finally we have implemented a custom validator.
   
   constructor(public vms_api:VmsApiService, public route:Router) { 
 
     this.CurrencyList=["USD","INR","GBP","EUR", "AUD","CAD","SGD"]; 
+    this.button_text="Submit"; 
   }
  
   ngOnInit() {
@@ -52,10 +57,13 @@ export class MyProfileComponent implements OnInit {
     this.vms_api.getUserDetails(user_id).subscribe(data=>{
       var result=JSON.parse(JSON.stringify(data));
       if(result.http_response_code==200){
+        console.log(result); 
         this.CompanyNameControl.setValue(result.data.company_name); 
         this.CompanyEmailControl.setValue(result.data.email);
         this.CurrencyControl.setValue(result.data.currency); 
         this.PlanControl.setValue("plan_"+result.data.opted_plan);
+        this.PayPalEmailControl.setValue(result.data.paypal_email); 
+        this.company_logo=this.vms_api.image_dir+"/company_logos/"+result.data.company_logo;
       }
       else{
         this.alertClass="alert alert-danger";
@@ -66,16 +74,16 @@ export class MyProfileComponent implements OnInit {
     error=>{
       console.log(error); 
     });
-
-
-    
-
     
   }
 
+  CompanyImageUpload(event){
+    this.company_logo_temp=event.target.files[0]; 
+  }
 
-  saveProfileDetails(){
 
+  SaveProfileDetails(){
+     //this.vms_api.save_profile_details(this.CompanyNameControl.value,this.CompanyEmailControl.value)
   }
 
 
